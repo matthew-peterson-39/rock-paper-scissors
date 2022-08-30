0b97a9029650be6a6c1a4d66f7b8e25d7aa06406
Matthew Peterson <matthew.peterson.39@hotmail.com>
2022-08-30 15:28:35 +0300

    Added a bit more responsiveness to the rounds. Added JS to inform the user of the winner each round and the overall winner of the game without using alerts.

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ const choices = document.querySelectorAll('button');
 const rock_btn = document.querySelector('#rock');
 const paper_btn = document.querySelector('#paper');
 const scissors_btn = document.querySelector('#scissors');
+const winner_block = document.querySelector('#fade-in-win');
+const winner_text = document.querySelector('#round-winner');
+const body = document.querySelector('body');
+const winner_div = document.createElement('div');
+const the_winner = document.createElement('h1');
+
 
 choices.forEach((choice) => {
     // and for each one we add a 'click' listener
-    choice.addEventListener('click', () => {
+    choice.addEventListener('click', () => { 
         user_choice = choice.id;
         cpu_choice = select_cpu();
         get_winner(user_choice, cpu_choice);
@@ -73,41 +79,75 @@ function get_winner(user, cpu) {
 }
 
 function cpu_win() {
-    alert('You lose!');
+    enlarge_text('cpu');
     update_score('cpu');
     return;
 }
 
 function user_win() {
-    alert('You won!');
+    enlarge_text('user');
     update_score('user');
     return;
 }
 
 function stale_mate() {
-    alert('Tie! No winner this round.')
+    enlarge_text('tie');
     return;
+}
+
+function enlarge_text(textToEnlarge) {
+    if(textToEnlarge == 'cpu') {
+        winner_text.innerText  = 'Result: The CPU Won!';
+    }
+    if(textToEnlarge == 'user') {
+        winner_text.innerText = 'Result: You Won!';
+    }
+    if (textToEnlarge == 'tie') {
+        winner_text.innerText = 'Result: Tie!';
+    }
 }
 
 function update_score(winner) {
     if (winner === 'cpu') {
         cpu_score.textContent = +cpu_score.textContent + 1;
         if (+cpu_score.textContent === 5) {
-            alert("You lost!");
+            display_winner("The Computer Won!");
             cpu_score.textContent = 0;
             user_score.textContent = 0;
+            winner_text.innerText = "Round Result: ";
             return;
         }
     }
     if (winner === 'user') {
         user_score.textContent = +user_score.textContent + 1;
         if (+user_score.textContent === 5) {
-            alert("You won!");
+            display_winner('You Won!');
             cpu_score.textContent = 0;
             user_score.textContent = 0;
+            winner_text.innerText = "Round Result: ";
             return;
         }
     }
-
-    
 }
+
+function display_winner(game_result) {
+    the_winner.innerText = `${game_result}`;
+    the_winner.style.fontSize = '64px';
+    winner_div.style.width = '100%';
+    winner_div.style.backgroundColor = 'black';
+    winner_div.style.display = 'flex';
+    winner_div.style.justifyContent = 'center';
+    winner_div.style.alignItems = 'center';
+
+    winner_div.appendChild(the_winner);
+
+    body.append(winner_div);
+}
+
+function reset_winner() {
+    if(winner_div.removeChild(winner_text)){
+        winner_text.innerText = " ";
+    }
+    return;
+}
+
